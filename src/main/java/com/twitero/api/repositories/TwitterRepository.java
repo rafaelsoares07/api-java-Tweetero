1428a631b17e29d710ad454ca82cec43bd837c11
rafaelsoares07 <rafaelsoares1936@gmail.com>
2023-01-23 18:37:36 -0300

    Renomeia model de Tweet dentro do repository

--- src/main/java/com/twitero/api/repositories/TwitterRepository.java
+++ src/main/java/com/twitero/api/repositories/TwitterRepository.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.twitero.api.model.Twitter;
+import com.twitero.api.model.Tweet;
 
-public interface TwitterRepository extends JpaRepository<Twitter,Long>{
-    List<Twitter> findByUsername(String username); 
+public interface TwitterRepository extends JpaRepository<Tweet,Long>{
+
+    List<Tweet> findByUsername(String username);
+    
 }
