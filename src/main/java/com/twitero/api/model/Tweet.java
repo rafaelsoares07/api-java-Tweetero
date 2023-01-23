c497c9a36fcb54e73b18e9d031ead9dae562da8f
rafaelsoares07 <rafaelsoares1936@gmail.com>
2023-01-23 18:36:04 -0300

    Cria Model de Tweet

---
+++ src/main/java/com/twitero/api/model/Tweet.java
@@ -0,0 +1,38 @@
+package com.twitero.api.model;
+
+import com.twitero.api.dto.TwitterDTO;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+public class Tweet {
+
+    public Tweet (TwitterDTO data, String avatar){
+        this.username = data.username();
+        this.tweet = data.tweet();
+        this.avatar = avatar;
+    }
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(length = 20, nullable = false)
+    private String username;
+
+    @Column(length = 250, nullable = false)
+    private String tweet;
+
+    @Column(length = 250000, nullable = true)
+    private String avatar;
+
+    
+}
