4aa060572aaa6b2ff378bda5e04d09e03f41adf4
rafaelsoares07 <rafaelsoares1936@gmail.com>
2023-01-23 18:36:31 -0300

    Refactor no controller do User

--- src/main/java/com/twitero/api/controllers/UserController.java
+++ src/main/java/com/twitero/api/controllers/UserController.java
@@ -1,10 +1,8 @@
 package com.twitero.api.controllers;
 
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
