014112b9bf06442e734d1828a298be03aafdcc80
rafaelsoares07 <rafaelsoares1936@gmail.com>
2023-01-23 17:19:29 -0300

    Cria repository JPA da entidade User

---
+++ src/main/java/com/twitero/api/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.twitero.api.repositories;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.twitero.api.model.User;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+
+}
