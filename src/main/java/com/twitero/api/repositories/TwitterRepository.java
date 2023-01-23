6c53a6ad01adabc2b5c163177574664f0bf34ae0
rafaelsoares07 <rafaelsoares1936@gmail.com>
2023-01-23 20:53:59 -0300

    Adiciona função que pega todos os tweets em ordem decrescente

--- src/main/java/com/twitero/api/repositories/TwitterRepository.java
+++ src/main/java/com/twitero/api/repositories/TwitterRepository.java
@@ -2,6 +2,8 @@ package com.twitero.api.repositories;
 
 import java.util.List;
 
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+import org.springframework.data.domain.Page;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -10,5 +12,7 @@ import com.twitero.api.model.Tweet;
 public interface TwitterRepository extends JpaRepository<Tweet,Long>{
 
     List<Tweet> findByUsernameOrderByIdDesc(String username);
+
+    Page<Tweet> findAllByOrderByIdDesc(org.springframework.data.domain.Pageable pageable);
     
 }
