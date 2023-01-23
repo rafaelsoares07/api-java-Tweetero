7b57d196792263c61030834daa6c18122d331eed
rafaelsoares07 <rafaelsoares1936@gmail.com>
2023-01-23 17:20:06 -0300

    Adiciona função findByUserName ao repository de User

--- src/main/java/com/twitero/api/repositories/UserRepository.java
+++ src/main/java/com/twitero/api/repositories/UserRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.twitero.api.model.User;
 
 public interface UserRepository extends JpaRepository<User,Long> {
-
+    User findByUsername(String username);
 }
