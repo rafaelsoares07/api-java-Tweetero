649d1c989ad3779ce960c892d63de9442b3fa75f
rafaelsoares07 <rafaelsoares1936@gmail.com>
2023-01-23 19:29:13 -0300

    Adiciona função de pegar tweets de um usúario ordenados

--- src/main/java/com/twitero/api/repositories/TwitterRepository.java
+++ src/main/java/com/twitero/api/repositories/TwitterRepository.java
@@ -2,12 +2,13 @@ package com.twitero.api.repositories;
 
 import java.util.List;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.twitero.api.model.Tweet;
 
 public interface TwitterRepository extends JpaRepository<Tweet,Long>{
 
-    List<Tweet> findByUsername(String username);
+    List<Tweet> findByUsernameOrderByIdDesc(String username);
     
 }
