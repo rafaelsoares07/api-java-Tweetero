1794d52daa93e461136eebbf1e1485b3779b50eb
rafaelsoares07 <rafaelsoares1936@gmail.com>
2023-01-23 17:17:45 -0300

    Cria repository JPA da entidade Tweet

---
+++ src/main/java/com/twitero/api/repositories/TwitterRepository.java
@@ -0,0 +1,11 @@
+package com.twitero.api.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.twitero.api.model.Twitter;
+
+public interface TwitterRepository extends JpaRepository<Twitter,Long>{
+     
+}
