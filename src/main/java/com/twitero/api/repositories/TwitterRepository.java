8996c24386cd89e5cdb473a7238f91d911373653
rafaelsoares07 <rafaelsoares1936@gmail.com>
2023-01-23 17:18:37 -0300

    Adiciona nova função ao repository de Tweet

--- src/main/java/com/twitero/api/repositories/TwitterRepository.java
+++ src/main/java/com/twitero/api/repositories/TwitterRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.twitero.api.model.Twitter;
 
 public interface TwitterRepository extends JpaRepository<Twitter,Long>{
-     
+    List<Twitter> findByUsername(String username); 
 }
